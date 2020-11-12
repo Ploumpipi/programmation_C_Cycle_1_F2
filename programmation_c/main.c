74629bb1882afb94b781856a93a32439be56cdab
Ploumpipi <56996716+Ploumpipi@users.noreply.github.com>
2020-11-12 12:57:56 +0100

    Suite du cours
    Ajout du cours jusqu'à la pause de midi

--- programmation_c/main.c
+++ programmation_c/main.c
@@ -3,6 +3,35 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    /* Commentaire paragraphe */
+    //Commentaire ligne
+
+    //unsigned : non signé, ex : unsigned short sn_entier
+    //integer
+    short sn_entier;
+    int n_entier;
+    long l_entier;
+
+    printf("Taille en octet du short : %d\n",sizeof(sn_entier));
+    printf("Taille en octet de entier : %d\n",sizeof(n_entier));
+    printf("Taille en octet du long : %d\n",sizeof(l_entier));
+
+    //reel
+    //unsigned : non signé, ex : unsigned float sn_entier
+    float flt_reel;
+    double dbl_reel;
+    long double ldbl_reel;
+
+    printf("Taille en octet du float : %d\n",sizeof(flt_reel));
+    printf("Taille en octet de double : %d\n",sizeof(dbl_reel));
+    printf("Taille en octet du long double : %d\n",sizeof(ldbl_reel));
+
+    //caractère
+    char c_car;
+    printf("Taille en octet du caractere : %d\n",sizeof(c_car));
+
     return 0;
+    //void = procédure
+    //int = fonction
+    //
 }
