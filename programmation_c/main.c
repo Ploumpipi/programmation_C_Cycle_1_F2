e0412e1923a5533f88d4fd3a24a224b05caaf4ed
Ploumpipi <56996716+Ploumpipi@users.noreply.github.com>
2020-11-17 14:38:48 +0100

    début jeu de l'oie

--- programmation_c/main.c
+++ programmation_c/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define ARRIVEE 66
+#define EXCEPTION 63
+#define MORT 58
+#define MULTIPLE 9
+#define VALMIN 2
+#define VALMAX 12
+#define DEPART 0
+
+int place, jetdedes;
+char car;
 
 int main()
 {
-    printf("Hello world !\n");
+    place=DEPART;
+
     return 0;
 }
