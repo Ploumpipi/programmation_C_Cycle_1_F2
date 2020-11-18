e0b3df189495691660e21af1f34627ed37b6adf3
Ploumpipi <56996716+Ploumpipi@users.noreply.github.com>
2020-11-18 10:10:20 +0100

    V1

--- programmation_c/main.c
+++ programmation_c/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #define ARRIVEE 66
 #define EXCEPTION 63
 #define MORT 58
@@ -8,12 +9,52 @@
 #define VALMAX 12
 #define DEPART 0
 
-int place, jetdedes;
-char car;
-
 int main()
 {
-    place=DEPART;
-
+    int place, jetDeDes;
+    srand(time(NULL));
+    while(place != ARRIVEE){
+        printf("joueur :\n");
+        lanceDeDe(&jetDeDes);
+        positionJoueur(&place,&jetDeDes);
+        printf("Obtient un %d", jetDeDes);
+        printf(" et se deplace a la case %d\n", place);
+    }
     return 0;
+}
+
+void lanceDeDe(int*jetDeDes)
+{
+    *jetDeDes=(rand()%(12-2+1))+2;
+}
+
+void positionJoueur(int *position, int *jetDeDes)
+{
+    *position = *position + *jetDeDes;
+    int nbrcase;
+    if(*position%9 == 0)
+    {
+        *position = *position+*jetDeDes;
+        printf("C'est une case oie, le lance compte double\n");
+    }
+    if (*position == EXCEPTION)
+    {
+        *position = EXCEPTION;
+    }
+    if (*position == MORT)
+    {
+        *position = DEPART;
+        printf("C'est la case mort, retour a la case depart\n");
+    }
+    if (*position>ARRIVEE)
+    {
+        *position = ARRIVEE - nbrcase;
+        nbrcase=*position-ARRIVEE;
+    }
+}
+
+void victoire (int position)
+{
+    if (position == ARRIVEE)
+        printf("Bravo vous avez gagne\n");
 }
