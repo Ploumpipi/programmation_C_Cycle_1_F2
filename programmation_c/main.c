270e83e3ab09802d25cb1e3a89ca3da9a858ff6c
Ploumpipi <56996716+Ploumpipi@users.noreply.github.com>
2020-11-18 14:52:24 +0100

    V1

--- programmation_c/main.c
+++ programmation_c/main.c
@@ -1,8 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define NBVIE 5
 
 int main()
 {
-    printf("Hello world !\n");
+    char mot, motve, motemp, ccar;
+    int vie;
+
+    vie=NBVIE;
+    mot = ' ';
+    printf("Quel est votre mot?\n");
+    scanf("%s",&mot);
+    //printf("%s",&mot);
+    motve=generec('_',length(mot));
+    while((vie>0)&&(motve!=mot)){
+        printf("%s",&motve);
+        scanf("%c",&ccar);
+        motemp=remplace(mot,motve,ccar);
+        if(motemp=motve){
+            vie=(vie-1);
+        }else{
+            motve=motemp;
+        }
+    }
+    if (vie=0){
+        printf("Perdu");
+    }else{
+        printf("Gagne");
+    }
     return 0;
+}
+
+void generec(char carac, int nb)
+{
+    char mot;
+    int i;
+    mot=' ';
+    for(i=0; i=nb; i++)
+    {
+        mot=mot+carac;
+    }
+    return mot;
+}
+
+void remplace(char mot1[], char mot2, char car)
+{
+    char temp;
+    int i;
+    temp=mot2;
+    for(i=0; i=length(mot1); i++)
+    {
+        if(mot1[i]=car)
+        {
+            temp[i]=car;
+        }
+        return temp;
+    }
 }
