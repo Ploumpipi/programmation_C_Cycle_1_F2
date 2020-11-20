1ae43df67bad77e30ccd8c9fde39102fd7c97f6e
Ploumpipi <56996716+Ploumpipi@users.noreply.github.com>
2020-11-20 10:29:45 +0100

    Les structures
    Typedef en C
    Exemple typedef tPoint
    Exemple pointeur sur typedef

--- programmation_c/main.c
+++ programmation_c/main.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Exemple un point dans un plan
+
+typedef struct tPoint{
+    int x;
+    int y;
+}tPoint;
+
 int main()
 {
-    printf("Hello world !\n");
+    tPoint monPoint;
+    tPoint *pMonPoint=NULL;
+    pMonPoint=&monPoint;
+
+    monPoint.x=10;
+    monPoint.y=5;
+
+    //Pointeur
+    pMonPoint->x=12;//pour un pointeur on remplace le point par une flèche
+    pMonPoint->y=20;
+
+    //formalisme pointeur
+    (*pMonPoint).x=20;
+    (*pMonPoint).y=12;
+
     return 0;
 }
