12ba81e4c3157f8627164fbc2f8c89d84d6ff7b7
Ploumpipi <56996716+Ploumpipi@users.noreply.github.com>
2020-11-25 10:58:24 +0100

    SDL2
    Init SDL2
    Window
    Renderer
    SDL_Delay
    SDL_Quit

--- programmation_c/main.c
+++ programmation_c/main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <SDL2/SDL.h>
+#define WINDOW_WIDTH 1024
+#define WINDOW_HEIGHT 780
 
-int main()
+SDL_Window *pWindow=NULL;
+SDL_Renderer *pRenderer=NULL;
+
+
+int main(int argc, char *argv[])
 {
-    printf("Hello world !\n");
+    if(SDL_Init(SDL_INIT_EVERYTHING)!=0){
+       SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+       return 1;
+       }else{
+        pWindow=SDL_CreateWindow("An SDL2 window", 0,0, WINDOW_WIDTH, WINDOW_HEIGHT,SDL_WINDOW_SHOWN);
+        if(pWindow){
+            pRenderer=SDL_CreateRenderer(pWindow, -1, SDL_RENDERER_PRESENTVSYNC);
+        }
+       }
+    SDL_Delay(10000);
+    SDL_Quit();
+
     return 0;
 }
