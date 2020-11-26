ebffa8a931b32fc51a3b6da76a7dd36789962b68
Ploumpipi <56996716+Ploumpipi@users.noreply.github.com>
2020-11-26 09:08:02 +0100

    SDL2

--- programmation_c/main.c
+++ programmation_c/main.c
@@ -14,12 +14,31 @@ int main(int argc, char *argv[])
        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
        return 1;
        }else{
-        pWindow=SDL_CreateWindow("An SDL2 window", 0,0, WINDOW_WIDTH, WINDOW_HEIGHT,SDL_WINDOW_SHOWN);
+        //An SDL2 window = titre de la fenêtre
+        //250 = position x et y
+        pWindow=SDL_CreateWindow("An SDL2 window", 250, 250, WINDOW_WIDTH, WINDOW_HEIGHT,SDL_WINDOW_SHOWN);
         if(pWindow){
             pRenderer=SDL_CreateRenderer(pWindow, -1, SDL_RENDERER_PRESENTVSYNC);
         }
        }
+    if(pRenderer){
+        //Set Color
+        SDL_SetRenderDrawColor(pRenderer, 205, 92, 92, SDL_ALPHA_OPAQUE);
+        //Clear Render
+        SDL_RenderClear(pRenderer);
+        //Update rendu (render)
+        SDL_RenderPresent(pRenderer);
+    }
     SDL_Delay(10000);
+
+    if(pRenderer){//Si pRenderer a une référence je la détruit
+        SDL_DestroyRenderer(pRenderer);
+    }
+    if(pWindow){// Si pWindow a une référence je la détruit
+        SDL_DestroyWindow(pWindow);
+    }
+    //SDL_DestroyRenderer(pRenderer);
+    //SDL_DestroyWindow(pWindow);
     SDL_Quit();
 
     return 0;
