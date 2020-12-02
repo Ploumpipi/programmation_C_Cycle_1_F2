d5364f4f1bbdede19cac97c1d41c2b1d4a4d6325
Ploumpipi <56996716+Ploumpipi@users.noreply.github.com>
2020-12-02 15:30:44 +0100

    ajout numéro des lignes et colonnes

--- programmation_c/main.c
+++ programmation_c/main.c
@@ -8,27 +8,32 @@ typedef struct pTab{
 }pTab;
 int main()
 {
-    /*char tabMap[TAILLE1][TAILLE2];
-    int x=0,y=0;
-
-    for (x=0;x<TAILLE1;x++){
-        for(y=0;y<TAILLE2;y++){
-            tabMap[x][y]="X";
-        }
-    }*/
     int MatriceEntiers2[TAILLE][TAILLE];//={{1,2,3,4},{5,6,7,8},{9,10,11,12}};
-    int i=0,j=0;
+    int i=0,j=0,nbx=0,nby=1;
     //Bordure de la zone d'eau
     //Création zone d'eau
     for(i=0;i<TAILLE;i++){//ligne
         for(j=0;j<TAILLE;j++){//colonne
             MatriceEntiers2[i][j]=1;
-            //printf("%d\n",MatriceEntiers2[n_i][n_j]);
+            MatriceEntiers2[0][j]=2;
+            MatriceEntiers2[nby][0]=3;
             if(MatriceEntiers2[i][j]==1){
-               printf("X");
+               printf("X ");
             }
             if(MatriceEntiers2[i][TAILLE-1]==1){
-                printf("X\n");
+                printf("X \n");
+            }
+            if(MatriceEntiers2[i][j]==2){
+                printf("%d ",nbx);
+                nbx++;
+            }
+            if(MatriceEntiers2[i][TAILLE-1]==2){
+                printf("%d \n",nbx);
+                nbx++;
+            }
+            if(MatriceEntiers2[i][j]==3){
+                printf("%d ",nby);
+                nby++;
             }
         }
     }
