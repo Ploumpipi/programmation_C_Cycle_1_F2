328af3c05f1c4cb62ba14fb9cd362a4142056a4b
Ploumpipi <56996716+Ploumpipi@users.noreply.github.com>
2020-12-04 17:08:31 +0100

    modif define

---
+++ programmation_c/define.h
@@ -0,0 +1,19 @@
+#ifndef DEFINE_H_INCLUDED
+#define DEFINE_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define TAILLE 11
+#define PAVION 5
+#define CROISEUR 4
+#define CTORPILLEUR1 3
+#define CTORPILLEUR2 3
+#define TORPILLEUR 2
+#define NBAT 5
+
+typedef struct posBat{
+    int x,y;
+}posBat;
+
+
+#endif // DEFINE_H_INCLUDED
